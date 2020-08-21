def9171878701bb9db7d563a4e67f0355ffe8a93
kyowill <291453783@qq.com>
2020-08-21 20:17:17 +0800

    commiT

--- src/main/java/com/Solution.java
+++ src/main/java/com/Solution.java
@@ -2,35 +2,24 @@ package com;
 import java.util.*;
 public class Solution {
 
-    public int maxDistance(int[] position, int m) {
-        Arrays.sort(position);
-        int l = 0;
-        int r = position[position.length-1] - position[0] + 1;
-        //f(l-1) >= m, f(r) < m
-        while (l < r) {
-            int mid = l + (r-l)/2;
-            if(count(position, mid) < m) {
-                r = mid;
-            }else {
-                l = mid + 1;
-            }
-        }
-        return l-1;
-    }
-
-    int count(int[] position, int d){
-        int ans = 1;
-        int cur = position[0];
-        for(int i = 1; i < position.length; i++){
-            if(position[i] - cur >= d){
-                ans++;
-                cur = position[i];
-            }
-        }
-        return ans;
-    }
 
     public static void main(String[] args) {
         System.out.println("hello");
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        System.out.println("sleep is interrupt");
+                        //break;
+                    }
+                }
+
+            }
+        });
+        t.start();
+        t.interrupt();
     }
 }
